7b99bd9aecf93a3e4713d0d9e80271047a48004c
juanv87 <juanv87@gmail.com>
2022-08-12 10:33:15 -0300

    first

---
+++ assets/js/header.js
@@ -0,0 +1,6 @@
+const hamb = document.querySelector(".hamb");
+const navMenu = document.querySelector(".nav-menu");
+
+hamb.addEventListener("click", function () {
+  navMenu.classList.toggle("hidden");
+});
