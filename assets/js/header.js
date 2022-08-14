90f3b1190a9f60a8b8e00e4344963b2a45c53e13
Juan <juanv87@gmail.com>
2022-08-13 21:31:26 -0300

    algunas cosas de js

--- assets/js/header.js
+++ assets/js/header.js
@@ -4,3 +4,12 @@ const navMenu = document.querySelector(".nav-menu");
 hamb.addEventListener("click", function () {
   navMenu.classList.toggle("hidden");
 });
+
+const headerRef = document.querySelector("section.header");
+window.addEventListener("scroll", function () {
+  if (window.scrollY > 95) {
+    headerRef.classList.add("scrolled");
+  } else {
+    headerRef.classList.remove("scrolled");
+  }
+});
