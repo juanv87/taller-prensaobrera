6f577edbb2d1ba4d563794ddfd1443073a99cf16
juanv87 <juanv87@gmail.com>
2022-08-23 17:40:19 -0300

    Avances js

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,15 +1,35 @@
 const hamb = document.querySelector(".hamb");
 const navMenu = document.querySelector(".nav-menu");
-
+const userAvatar = document.querySelector(".user-avatar");
 hamb.addEventListener("click", function () {
   navMenu.classList.toggle("hidden");
 });
 
-const headerRef = document.querySelector("section.header");
-window.addEventListener("scroll", function () {
-  if (window.scrollY > 95) {
-    headerRef.classList.add("scrolled");
+userAvatar.addEventListener("click", () => {
+  navMenu.classList.toggle("hidden");
+});
+
+let tarjeta = document.querySelector(".notas-4-sin-foto .politicas");
+let tituloTarjeta = document.querySelector(".notas-4-sin-foto .politicas h3");
+
+tarjeta.addEventListener("click", () => {
+  tarjeta.classList.toggle("fondoRojo");
+  if (tituloTarjeta.innerText === "Hola") {
+    tituloTarjeta.innerText = "Chau";
   } else {
-    headerRef.classList.remove("scrolled");
+    tituloTarjeta.innerText = "Hola";
   }
 });
+
+document.querySelector("#hola").addEventListener("click", () => {
+  alert("Adios");
+});
+
+// const headerRef = document.querySelector("section.header");
+// window.addEventListener("scroll", function () {
+//   if (window.scrollY > 95) {
+//     headerRef.classList.add("scrolled");
+//   } else {
+//     headerRef.classList.remove("scrolled");
+//   }
+// });
