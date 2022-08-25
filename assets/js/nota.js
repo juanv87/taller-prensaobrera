1a58b41fd76b29c89c4c7d5b32badd528208efef
juanv87 <juanv87@gmail.com>
2022-08-25 18:15:51 -0300

    avaces

--- assets/js/nota.js
+++ assets/js/nota.js
@@ -1,7 +1,15 @@
 const refButton = document.querySelector("#saveTitle");
 const refInputTitle = document.querySelector("#inputTitle");
 const refNotaTitle = document.querySelector("h1");
+const nuevaNota = document.querySelector("#nuevaNota");
 
 refButton.addEventListener("click", function () {
-  refNotaTitle.innerText = refInputTitle.value;
+  console.log(refNotaTitle.innerText);
+  const nota = {
+    titulo: refNotaTitle.innerText,
+  };
+  console.log({ nota });
+  nuevaNota.innerHTML = `
+    <p> ${nota.titulo} </p>
+  `;
 });
